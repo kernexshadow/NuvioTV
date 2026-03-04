d611fa2dcb6e7d23ba33692ad9ee10e5d87ed89e
tapframe <85391825+tapframe@users.noreply.github.com>
2026-03-05 02:49:19 +0530

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.4.9-beta"
+        versionCode = 28
+        versionName = "0.4.10-beta"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
